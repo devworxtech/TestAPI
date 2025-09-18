bcbdfcd2af94c1324a1f8ebc02ee7906522fd91c
devworxtech <devworx.tech@gmail.com>
2025-09-18 20:14:19 +0300

    test

--- LogTestAPI/Program.cs
+++ LogTestAPI/Program.cs
@@ -19,7 +19,7 @@ using var logger = new LoggerConfiguration()
     .CreateLogger();
 
 builder.Logging.AddSerilog(logger);
-for (int i = 0; i < 50_000; i++)
+for (int i = 0; i < 100; i++)
 {
     logger.Debug("Starting up");
 }
